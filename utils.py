6e77122957ca3c3bf17c79b23d9992fbec7eeb75
davidgarciasanz01 <126099334+davidgarciasanz01@users.noreply.github.com>
2025-03-18 21:57:18 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,13 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 from langchain_pinecone import PineconeEmbeddings
 import re
-
-
-
-from langchain_community.embeddings import OllamaEmbeddings
+import asyncio
 
 def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url="http://tuservidor:11434")
+    # Inicializar embeddings con Pinecone
+    embeddings = PineconeEmbeddings(
+        model="multilingual-e5-large",  # Modelo de embeddings
+        pinecone_api_key="c563e341-f430-41a5-8dc4-93596352b778",  # Reemplaza con tu API key de Pinecone
+        index_name="MAN"          # Nombre del índice que creaste en Pinecone
+    )
     return embeddings
-
-
