b1e2dae06d603580b09f896f241a025784f330de
davidgarciasanz01 <126099334+davidgarciasanz01@users.noreply.github.com>
2025-03-18 21:38:59 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
-#from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 from langchain_pinecone import PineconeEmbeddings
 import re
-from langchain_ollama import OllamaEmbeddings
+
 
 
 import asyncio
