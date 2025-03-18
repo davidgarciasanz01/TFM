c01d8da4de13e6040126f6a91872df6c723a78a6
davidgarciasanz01 <126099334+davidgarciasanz01@users.noreply.github.com>
2025-03-18 22:03:27 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,13 +2,21 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 from langchain_pinecone import PineconeEmbeddings
+
 import re
 import asyncio
 
+
 def get_embedding_function():
-    # Inicializar embeddings con Pinecone
-    embeddings = PineconeEmbeddings(
-        model="multilingual-e5-large",  # Modelo de embeddings
-        pinecone_api_key="c563e341-f430-41a5-8dc4-93596352b778"        
-    )
+    # Asegurar que hay un bucle de eventos activo
+    try:
+        asyncio.get_running_loop()
+    except RuntimeError:
+        asyncio.set_event_loop(asyncio.new_event_loop())
+
+    # Inicializar embeddings
+    #embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    embeddings = PineconeEmbeddings(model="multilingual-e5-large")
     return embeddings
+
+
