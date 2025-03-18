4dcc3e198cb2363a0bd06f030eb26674918b49b1
davidgarciasanz01 <126099334+davidgarciasanz01@users.noreply.github.com>
2025-03-18 22:02:06 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,6 @@ def get_embedding_function():
     # Inicializar embeddings con Pinecone
     embeddings = PineconeEmbeddings(
         model="multilingual-e5-large",  # Modelo de embeddings
-        pinecone_api_key="c563e341-f430-41a5-8dc4-93596352b778",  # Reemplaza con tu API key de Pinecone
-        index_name="man2"          # Nombre del índice que creaste en Pinecone
+        pinecone_api_key="c563e341-f430-41a5-8dc4-93596352b778"        
     )
     return embeddings
